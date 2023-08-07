d515c32a07b48e31e2a6c9f7c2e593c775fd3973
Yassine Chebbi <yassine.chebbi2023@gmail.com>
2023-08-07 18:38:26 +0100

    project_structure

---
+++ src/main/java/com/yassine/todo/TodoApplication.java
@@ -0,0 +1,13 @@
+package com.yassine.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
